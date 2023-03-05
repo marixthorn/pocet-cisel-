85479848fe80b8c82b4096b386a46a879a3194f2
domin <domin@DESKTOP-UN5POIO>
2023-03-05 18:07:22 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Zadej čísla oddělená čárkou: ");
+string input = Console.ReadLine() ?? "";
+string[] numbers = input.Split(',');
+numbers = numbers.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+Console.WriteLine($"\nPočet čísel: {numbers.Length}");
